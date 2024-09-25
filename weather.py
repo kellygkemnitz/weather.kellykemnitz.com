206f8dc3a6201bcec2cbd9326e7f2e5a1a7aad38
Kelly Kemnitz <kellygkemnitz@gmail.com>
2024-09-25 16:31:07 -0500

    Updates every 5 minutes

--- weather.py
+++ weather.py
@@ -1,7 +1,10 @@
 import dash
 from dash import dcc, html
+from dash.dependencies import Output, Input
 
 from apscheduler.schedulers.background import BackgroundScheduler
+
+import pandas as pd
 
 from scrape_wunderground import WeatherStation
 from plotly_graphs import create_temperature_dewpoint_graph, create_humidity_graph, create_wind_graph, create_rain_graph, create_pressure_graph
@@ -11,17 +14,24 @@ app = dash.Dash(__name__)
 
 ws = WeatherStation()
 
+df = pd.DataFrame()
+
 def update_data():
     global df
     df = ws.scrape_wunderground()
 
 scheduler = BackgroundScheduler()
-scheduler.add_job(update_data, 'interval', hours=1)
+scheduler.add_job(update_data, 'interval', minutes=5)
 scheduler.start()
 
 update_data()
 
 app.layout = html.Div(children=[
+    dcc.Interval(
+        id='interval-component',
+        interval=5*60*1000,  # in milliseconds
+        n_intervals=0
+    ),
     dcc.Graph(
         id='temperature-dewpoint-graph',
         figure=create_temperature_dewpoint_graph(df)
@@ -43,6 +53,22 @@ app.layout = html.Div(children=[
         figure=create_pressure_graph(df)
     )
 ])
+
+
+@app.callback(
+    [Output('temperature-dewpoint-graph', 'figure'),
+     Output('humidity-graph', 'figure'),
+     Output('wind-graph', 'figure'),
+     Output('rain-graph', 'figure'),
+     Output('pressure-graph', 'figure')],
+    [Input('interval-component', 'n_intervals')])
+def update_graphs(n):
+    return (create_temperature_dewpoint_graph(df),
+            create_humidity_graph(df),
+            create_wind_graph(df),
+            create_rain_graph(df),
+            create_pressure_graph(df))
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
