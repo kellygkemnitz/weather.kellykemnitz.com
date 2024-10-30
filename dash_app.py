86eb7528bb8d62e8b03c6155e5bd61242eda7537
Kelly Kemnitz <kellygkemnitz@gmail.com>
2024-10-30 10:10:29 -0500

    Added some more images

--- dash_app.py
+++ dash_app.py
@@ -64,26 +64,60 @@ app.layout = html.Div(
                 ]),
                 dbc.Tab(label='Radar', tab_id='tab-6', children=[
                     dbc.Row([
-                        dbc.Col(html.Div(html.Img(src="https://radar.weather.gov/ridge/standard/KICT_loop.gif"))),
-                        dbc.Col(html.Div(html.Img(src="http://sirocco.accuweather.com/nx_mosaic_640x480_public/sir/inmasirks_.gif")))
+                        dbc.Col(html.Div(html.Img(src="https://radar.weather.gov/ridge/standard/KICT_loop.gif")), width="auto"),
+                        dbc.Col(html.Div(html.Img(src="https://s.w-x.co/staticmaps/wu/wxtype/county_loc/sln/animate.png")), width="auto"),
+                        dbc.Col(html.Div(html.Img(src="http://sirocco.accuweather.com/nx_mosaic_640x480_public/sir/inmasirks_.gif")), width="auto")
                     ])
                 ]),
                 dbc.Tab(label='Watches/Warnings', tab_id='tab-7', children=[
                     dbc.Row([
-                        dbc.Col(html.Div(html.Img(src="http://www.spc.noaa.gov/products/watch/validww.png")))
+                        dbc.Col(html.Div(html.Img(src="http://www.spc.noaa.gov/products/watch/validww.png")), width="auto"),
+                        dbc.Col(html.Div(html.Img(src="https://www.weather.gov/wwamap/png/ict.png")), width="auto")
                     ])
                 ]),
                 dbc.Tab(label='Forecast', tab_id='tab-8', children=[
                     dbc.Row([
-                        html.Div(html.Img(src="https://media.psg.nexstardigital.net/ksnw/weather/images/wx_weekly_full.jpg"))
-                    ])
+                        dbc.Col(html.Div([
+                            html.H5("NOAA - Today's Forecast"),
+                            html.Img(src="https://www.wpc.ncep.noaa.gov/noaa/noaad1.gif", style={'width': '100%', 'height': 'auto'})
+                        ]), width=4),
+                        dbc.Col(html.Div([
+                            html.H5("NOAA - Tomorrow's Forecast"),    
+                            html.Img(src="https://www.wpc.ncep.noaa.gov/noaa/noaad2.gif", style={'width': '100%', 'height': 'auto'})
+                        ]), width=4),
+                        dbc.Col(html.Div([
+                            html.H5("NOAA - Day 3 Forecast"),    
+                            html.Img(src="https://www.wpc.ncep.noaa.gov/noaa/noaad3.gif", style={'width': '100%', 'height': 'auto'})
+                        ]), width=4)
+                    ]),
+                    dbc.Row(
+                        dbc.Col(html.Div([
+                            html.H5("KSN Forecast"),    
+                            html.Img(src="https://media.psg.nexstardigital.net/ksnw/weather/images/wx_weekly_full.jpg")
+                        ]), width=4)
+                    )
                 ]),
                 dbc.Tab(label='Outlook', tab_id='tab-9', children=[
                     dbc.Row([
-                        html.Div(html.Img(src="https://www.spc.noaa.gov/products/outlook/day1otlk_1300.gif")),
-                        html.Div(html.Img(src="https://www.spc.noaa.gov/products/outlook/day2otlk_0600.gif")),
-                        html.Div(html.Img(src="https://www.spc.noaa.gov/products/outlook/day3otlk_0730.gif"))
-                    ])
+                        dbc.Col(html.Div([
+                            html.H5("SPC Day 1 Outlook"),
+                            html.Img(src="https://www.spc.noaa.gov/products/outlook/day1otlk_1300.gif")
+                        ]), width=4),
+                        dbc.Col(html.Div([
+                            html.H5("SPC Day 2 Outlook"),
+                            html.Img(src="https://www.spc.noaa.gov/products/outlook/day2otlk_0600.gif")
+                        ]), width=4),
+                        dbc.Col(html.Div([
+                            html.H5("SPC Day 3 Outlook"),
+                            html.Img(src="https://www.spc.noaa.gov/products/outlook/day3otlk_0730.gif")
+                        ]), width=4)
+                    ]),
+                    dbc.Row(
+                        dbc.Col(html.Div([
+                            html.H5("ICT Hazardous Weather Outlook"),
+                            html.Img(src="https://www.weather.gov/images/ict/ghwo/SevereThunderstormsDay1.jpg")
+                        ]), width=4)
+                    )
                 ])
             ]
         )
